ee40427953b8d67ee40873f9e02549bcf15e739d
TashaStinger <104356141+TashaStinger@users.noreply.github.com>
2022-11-22 15:46:43 +0100

    Update Elements.js

--- src/Elements.js
+++ src/Elements.js
@@ -3,7 +3,19 @@ import React from "react";
 export default function Elements(props) {
     return(
         <div className="Elements">
-            Hello from Elements
+            <div className="row">
+                <div className="col">
+                    <iframe width="170" height="300" src="https://www.youtube.com/embed/YYXpu4vTCXk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div className="col">
+                   <iframe width="225" height="300" src="https://www.youtube.com/embed/BQ8oAoejdgU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                 <div className="col">
+                   <iframe width="225" height="300" src="https://youtube.com/embed/5TBCfSgQ6N0?feature=share" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+    
+    
         </div>
     )
 }
