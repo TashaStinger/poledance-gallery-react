3f85f5c5dcf3cb6b12ea087268734a69d2ccd677
TashaStinger <104356141+TashaStinger@users.noreply.github.com>
2022-12-05 09:50:58 +0100

    Fixed some warnings

--- src/Element.js
+++ src/Element.js
@@ -5,8 +5,8 @@ export default function Element(props) {
     return (
         <div className="Element">
             {/* <iframe width="130" height="200" src={props.element.url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
-            <a href={props.element.url} target="_blank">
-                <img src={props.element.img} width={100} />
+            <a href={props.element.url} target="_blank" rel="noreferrer">
+                <img src={props.element.img} width={100} alt={props.element.name} />
             </a>
             <br />
             {props.element.name}
--- src/Gallery.js
+++ src/Gallery.js
@@ -26,58 +26,39 @@ export default function Gallery() {
     if (loaded){
         return(
             <div className="Gallery">
-                {/* <p>
-                <button class="btn btn-light m-1" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                    База
-                </button>
-                <button class="btn btn-light m-1" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">
-                    Колінний зачіп
-                </button>
-                </p>
-                <div class="collapse" id="collapseExample">
-                <div class="card card-body">
-                    <Elements filter="type" filterValue="base"/>
-                </div>
-                </div>
-                <div class="collapse" id="collapseExample1">
-                <div class="card card-body">
-                    <Elements filter="type" filterValue="knee"/>
-                </div>
-                </div> */}
-
-                <div class="accordion" id="accordionExample">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingOne">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                         База
                     </button>
                     </h2>
-                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
                         <Elements filter="type" filterValue="base"/>
                     </div>
                     </div>
                 </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingTwo">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                         Колінний зачіп
                     </button>
                     </h2>
-                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
                         <Elements filter="type" filterValue="knee"/>
                     </div>
                     </div>
                 </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingThree">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                         Зачіп стегнами
                     </button>
                     </h2>
-                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
                         <Elements filter="type" filterValue="hips"/>
                     </div>
                     </div>
