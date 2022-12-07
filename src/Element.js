f2afeb0883751f265ea7523e46a264fe26ee4932
TashaStinger <104356141+TashaStinger@users.noreply.github.com>
2022-12-07 08:47:20 +0100

    fixed some warnings

--- src/Element.js
+++ src/Element.js
@@ -1,45 +1,13 @@
-import React, { useState } from "react";
-// import pic from "./images/005.jpeg";
+import React from "react";
 
 export default function Element(props) {
-    // let [clicked, setClicked] = useState(false);
-
-    // function loadVideo(event){
-    //     event.preventDefault();
-    //     setClicked(true);
-    // }
-
-    // if (clicked) {
-    //     return (
-    //         <div className="Element">
-    //             <div class="modal" tabindex="-1">
-    //             <div class="modal-dialog">
-    //                 <div class="modal-content">
-    //                 <div class="modal-header">
-    //                     <h5 class="modal-title">Modal title</h5>
-    //                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-    //                 </div>
-    //                 <div class="modal-body">
-    //                     <p>Modal body text goes here.</p>
-    //                 </div>
-    //                 <div class="modal-footer">
-    //                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-    //                     <button type="button" class="btn btn-primary">Save changes</button>
-    //                 </div>
-    //                 </div>
-    //             </div>
-    //             </div>
-    //         </div>
-    //     )
-    // } else {
-        console.log("#"+props.element.id);
         return (
             <div className="Element">
                 {/* <iframe width="130" height="200" src={props.element.url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
                 {/* <a href={props.element.url} target="_blank" rel="noreferrer" onClick={loadVideo}>
                     <img src={props.element.img} width={100} alt={props.element.name} />
                 </a> */}
-                                {/* <!-- Button trigger modal --> */}
+                {/* <!-- Button trigger modal --> */}
                 <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={"#"+props.element.id}>
                 {props.element.name}
                 </button>
@@ -65,6 +33,5 @@ export default function Element(props) {
                 {props.element.name} */}
             </div>
         )
-    // }
 
 }
