0a8de8617acd0f67529c2a1a4fb284acdf01d4c1
TashaStinger <104356141+TashaStinger@users.noreply.github.com>
2023-06-02 19:40:21 +0200

    Update Program.js

--- src/Program.js
+++ src/Program.js
@@ -89,39 +89,37 @@ export default function Program() {
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingThree">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Pole Exotic
+                        Stretching
                     </button>
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
-                        {/* <Elements filter="type" filterValue="hips" level={level} /> */}
-                        Pole Exotic Program
+                        <Stretching date={currentDate} />
                     </div>
                     </div>
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingFour">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                        Pole Art
+                        Pole Exotic
                     </button>
                     </h2>
                     <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
                         {/* <Elements filter="type" filterValue="handstand" level={level} /> */}
-                        Pole Art Program
+                        Pole Exotic Program
                     </div>
                     </div>
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingFive">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                        Stretching
+                        Pole Art
                     </button>
                     </h2>
                     <div id="collapseFive" className="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
-                        {/* <Elements filter="type" filterValue="spin" level={level} /> */}
-                        <Stretching date={currentDate} />
+                        Pole Art Program
                     </div>
                     </div>
                 </div>
